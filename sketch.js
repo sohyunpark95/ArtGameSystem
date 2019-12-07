9f0004697146a6ed937deaa8cb62cabee259eafc
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-07 21:12:24 +0900

    make_animation1

--- sketch.js
+++ sketch.js
@@ -3,7 +3,10 @@ let canvasWidth = 600;
 let canvasHeight = 400;
 let squid;
 let x, y;
+var tooth;
 
+function preload() {
+  tooth = loadAnimation('lib/mouth_image_1.jpg', 'lib/mouth_image_2.jpg','lib/mouth_image_3.jpg');
 
 function setup() {
   background(0,0,0);
@@ -18,7 +21,7 @@ function setup() {
 function draw() {
 //  rect(mouseX, mouseY,40,200);
 //stroke(50);
-
+animation(tooth, 600, 400);
 rect(0,300,140,100);
 rect(460,300,140,100);
 fill(255,240,180,250);
@@ -26,6 +29,7 @@ rect(320,300,140,100);
 rect(140,300,140,100);
 fill(255,255,255);
 rect(x, y, 40, 100);
+
 x = x ;
 // Moving up at a constant speed
 y = y + 1;
