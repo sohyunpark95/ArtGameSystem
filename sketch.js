e6ffbd4147ba8a8f4a8d7515aae8446a4ba3f4f4
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-07 22:21:04 +0900

    change backgroundcolor_makerect

--- sketch.js
+++ sketch.js
@@ -27,7 +27,8 @@ function setup() {
 function draw() {
 //  rect(mouseX, mouseY,40,200);
 //stroke(50);
-
+fill(255,255,255);
+rect(0,0,600,400)
 rect(0,300,140,100);
 rect(460,300,140,100);
 fill(255,240,180,250);
