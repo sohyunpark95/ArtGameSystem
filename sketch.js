1075a3c81c99caf28f5f07ab4efa762e64ca2101
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-17 02:01:42 +0900

    finding_resetGame

--- sketch.js
+++ sketch.js
@@ -2,7 +2,10 @@ let canvas;
 let canvasWidth = 1500;
 let canvasHeight = 400;
 let umbrella;
-let x, y;
+let stone;
+let chopstick;
+let Ry;
+let x;
 var tooth;
 
 
@@ -13,27 +16,46 @@ function preload() {
 function setup() {
   canvas = createCanvas(canvasWidth, canvasHeight);
   canvas.position(windowWidth/3 - canvasWidth/3, 20);
-  x = 280;
-  y = 0;
 
+  resetGame();
+  Ry = 0;
+x = 250;
 }
-
 function draw() {
 background(100,100,255,5);
 fill(100,100,120);
 rect(0,0,500,400);
 fill(255,240,180,250);
 rect(1000,0,500,400);
-line(30, 20, 85, 20);
-stroke(126);
-rect(x, y, 40, 100);
+line(0,200,280,300);
+line(0,210,280,310);
+line(320,310,500,380);
+line(320,320,500,390);
+line(280,300,280,310);
+line(320,310,320,320);
+fill(255,10,0);
 
-x = x ;
-y = y + 1;
-
-// Reset to the bottom
-if (y >= 300) {
-  y = 300;
-  rect(0,0,200,400);
+fill(50,50,255);
+quad(x, Ry, x+40, Ry+10, x+40, Ry+20, x, Ry+10 );
+Ry = Ry + 1;
+if (Ry >= 400) {
+rect(0,0,200,400);
+if (x <= 280, x >=280){
 }
+}
+}
+function resetGame() {
+
+}
+function keyPressed() {
+  if (keyCode === LEFT_ARROW) {
+    x = x-10;
+  } else if (keyCode === RIGHT_ARROW) {
+    x = x+10;
+  }
+}
+
+function resetGame(){
+
+x=0;
 }
