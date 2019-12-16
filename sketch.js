8e70b92dd3449a5fc3de83e2865792d80eb58298
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-16 23:07:40 +0900

    change background color

--- sketch.js
+++ sketch.js
@@ -19,24 +19,21 @@ function setup() {
 }
 
 function draw() {
-//  rect(mouseX, mouseY,40,200);
-//stroke(50);
-background(100,100,255,10);
-rect(0,300,140,100);
-rect(460,300,140,100);
+background(100,100,255,5);
+fill(100,100,120);
+rect(0,0,500,400);
 fill(255,240,180,250);
-rect(320,300,140,100);
-rect(140,300,140,100);
-fill(255,255,255);
+rect(1000,0,500,400);
+line(30, 20, 85, 20);
+stroke(126);
 rect(x, y, 40, 100);
 
 x = x ;
-// Moving up at a constant speed
 y = y + 1;
 
 // Reset to the bottom
 if (y >= 300) {
   y = 300;
-  animation(tooth,200,200);
+  rect(0,0,200,400);
 }
 }
