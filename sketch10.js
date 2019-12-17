e0051dd0bc7817815b34a14ad6d5d66bfdc41603
sohyunpark95 <57379451+sohyunpark95@users.noreply.github.com>
2019-12-17 14:11:54 +0900

    organizaition

--- sketch10.js
+++ sketch10.js
@@ -4,11 +4,8 @@ let canvasHeight = 400;
 let umbrella;
 let stone;
 let chopstick;
-let Rx, Ex, Cx;
-let y;
-let Rain;
-let Earth;
-let Concent;
+let Ry;
+let x;
 var tooth;
 
 
@@ -21,11 +18,8 @@ function setup() {
   canvas.position(windowWidth/3 - canvasWidth/3, 20);
 
   // resetGame();
-  y = 0;
-  Ex = 600;
-  Rx = 250;
-  Cx = 0;
-
+  Ry = 0;
+x = 250;
 }
 function draw() {
 background(100,100,255,5);
@@ -39,47 +33,25 @@ line(320,310,500,380);
 line(320,320,500,390);
 line(280,300,280,310);
 line(320,310,320,320);
+fill(255,10,0);
+
 fill(50,50,255);
-Rain = quad(Rx, Ry, Rx+40, Ry+10, Rx+40, Ry+20, Rx, Ry+10);
+quad(x, Ry, x+40, Ry+10, x+40, Ry+20, x, Ry+10 );
 Ry = Ry + 1;
 if (Ry >= 400) {
-  fill(255,150,150);
-triangle(500,400,750,350,1000,400);
-fill(100,100,255);
-rect(Ex,Ey,10,40);
-Ey = Ey + 1;
-if (Ey >= 350) {
-  fill(0);
-  rect(1450,200,50,50);
-  fill(255,150,150);
-rect(Cx,Cy,50,50);
-// Cy = Cy + 1;
+rect(500,200,500,300);
 // if (x <= 280, x >=280){
 // }
 }
 }
 // function resetGame() {
+
 }
 function keyPressed() {
   if (keyCode === LEFT_ARROW) {
-    Rx = Rx-10;
+    x = x-10;
   } else if (keyCode === RIGHT_ARROW) {
-    Rx = Rx+10;
-  }
-  if (keyCode === LEFT_ARROW) {
-    Ex = Ex-10;
-  } else if (keyCode === RIGHT_ARROW) {
-    Ex = Ex+10;
-  }
-  if (keyCode === LEFT_ARROW) {
-    Cx = Cx-10;
-  } else if (keyCode === RIGHT_ARROW) {
-    Cx = Cx+10;
-  }
-  if (keyCode === UP_ARROW) {
-    Cy = Cy-10;
-  } else if (keyCode === DOWN_ARROW) {
-    Cy = Cy+10;
+    x = x+10;
   }
 }
 
